6a188d352f3a46296b8682c507909547d4eb4862
MichalMakowiejczuk <126947935+MichalMakowiejczuk@users.noreply.github.com>
2024-02-24 11:02:58 +0100

    Update zad_1.py

--- zad_1.py
+++ zad_1.py
@@ -1,7 +1,9 @@
+imiona = ["Michał", "Paulina", "Adam", "Ania", "Piotrek"]
+
+
 def my_func(imiona):
     for imie in imiona:
         print(imie)
 
 
-imiona = ["Michał", "Paulina", "Adam", "Ania", "Piotrek"]
 my_func(imiona)
