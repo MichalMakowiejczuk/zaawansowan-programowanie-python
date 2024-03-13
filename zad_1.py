07f4e193d52da40d711ce6f4a043c500c1b1d926
MichalMakowiejczuk <126947935+MichalMakowiejczuk@users.noreply.github.com>
2024-03-13 12:03:05 +0100

    add zad_1 files

--- zad_1.py
+++
@@ -1,9 +0,0 @@
-imiona = ["Michał", "Paulina", "Adam", "Ania", "Hanna", "Julia"]
-
-
-def my_func(imiona):
-    for imie in imiona:
-        print(imie)
-
-
-my_func(imiona)
--- zad_4.py
+++
@@ -1,6 +0,0 @@
-def my_func(list_of_ints):
-    return list_of_ints[::2]
-
-
-test_ints = list(range(10))
-print(my_func(test_ints))
