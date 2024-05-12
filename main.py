4277a07eaffb7b5a782112a2b1284159b752f0c2
MichalMakowiejczuk <126947935+MichalMakowiejczuk@users.noreply.github.com>
2024-05-12 23:07:28 +0200

    1

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import cv2
 def detect_people(image_path):
     # Wczytanie obrazu
     image = cv2.imread(image_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Utworzenie detektora ludzi
     hog = cv2.HOGDescriptor()
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
     # Wykrywanie ludzi na obrazie
-    (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+    (rects, weights) = hog.detectMultiScale(gray, scale=1.09, winStride=(4, 8))
 
     # Zaznaczanie ludzi na obrazie
     for (x, y, w, h) in rects:
@@ -22,7 +23,7 @@ def detect_people(image_path):
     return len(rects), output_image_path
 
 if __name__ == "__main__":
-    image_path = "sample_image.jpg"  # Ścieżka do zdjęcia
+    image_path = "pic/sample3.jpg"  # Ścieżka do zdjęcia
     num_people, output_image_path = detect_people(image_path)
     print("Liczba wykrytych ludzi:", num_people)
     print("Zapisano obraz z zaznaczonymi ludźmi:", output_image_path)
